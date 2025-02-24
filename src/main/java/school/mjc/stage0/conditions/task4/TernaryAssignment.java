d3dc66cd36823b72486064fecd3d80440887dfe3
nodircha <madnodiras23@gmail.com>
2025-02-24 20:48:11 +0500

    stage0module4conditionstask4

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryAssignment.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryAssignment.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryAssignment {
     public void assignAndPrintBasedOnWhichBigger(int first, int second) {
+        int result=10;
+        result = first > second?
+                result:
+                -(result);
+        System.out.println(result);
     }
 }
